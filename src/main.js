78e405233c0878f0dd608353f22035ed935b9405
sureshwebosmotic <suresh.webosmotic@gmail.com>
2024-04-04 19:41:49 +0530

    finished crud operation using html, css and js.

--- src/main.js
+++ src/main.js
@@ -6,27 +6,5 @@ export default class Main {
 }
 window.onload = function () {
   new Main();
+  onLoad();
 };
-
-// var employees = JSON.parse(localStorage.getItem('employees')) || [];
-
-// function AddData() {
-
-//       var name = document.getElementById("name").value;
-//       var gender = document.getElementById("gender").value;
-//       var birthdate = document.getElementById("birthdate").value;
-//       var email = document.getElementById("email").value;
-//       var phone = document.getElementById("phone").value;
-//       var hobbies = document.getElementById("hobbies").value;
-//       console.log(hobbies);
-//       employees.push({
-//           name: name,
-//           gender: gender,
-//           birthdate: birthdate,
-//           email: email,
-//           phone: phone,
-//           hobbies: hobbies
-//       });
-
-//       localStorage.setItem('employees', JSON.stringify(employees));  
-// }
